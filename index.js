5e0f287d2f590673972d8d3339a5db9bac6ba4b9
JonathanHaws <jonathanmhaws1@gmail.com>
2023-02-04 19:47:50 -0700

    title and description

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 inquirer.prompt([
     { type: 'input', message: 'What is the title of your read me?', name: 'title',},
@@ -10,5 +11,13 @@ inquirer.prompt([
     { type: 'input', message: 'What is your Github Username',name: 'githubusername', },
     { type: 'input', message: 'What is your Email',name: 'email', },])
     .then(function(response){
+        let content;
+        content = '\n# '+ response.title + '\n\n'+
+        '## Description \n'+ response.description ;
+
         console.log(response.title);
+        fs.writeFile('README.md', content, function(error, file){
+            if (error) {console.log("Could not create file")}
+            else {console.log("File created successfully")}
+        });
     });
