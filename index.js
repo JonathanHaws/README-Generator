6966569ae4e627a726a4bb614dad742a7b99ceee
JonathanHaws <jonathanmhaws1@gmail.com>
2023-02-08 00:07:31 -0700

    table of contents, liscense, and fix headers

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ inquirer.prompt([
     { type: 'input', message: 'What is the description for your project', name: 'description',},
     { type: 'input', message: 'What are the instillation instructions for this repo?', name: 'instillationinstructions',},
     { type: 'input', message: 'What is the usage information?', name: 'usageinformation',},
-    { type: 'input', message: 'What are the contrubution guidelines?', name: 'contrubution guidelines',},
-    { type: 'input', message: 'What are the test instructions?', name: 'testinstructions',},
+    { type: 'list', message: 'What liscence do you want?', name: 'liscence', choices: ['MIT', 'CC',],},
+    { type: 'input', message: 'What are the contrubution guidelines?', name: 'contributing',},
+    { type: 'input', message: 'What are the test instructions?', name: 'test',},
     { type: 'input', message: 'What is your Github Username',name: 'githubusername', },
     { type: 'input', message: 'What is your Email',name: 'email', },])
     .then(function(response){
+        
         let content;
-        content = '\n# '+ response.title + '\n\n'+
-        '## Description \n'+ response.description +'\n\n'+
-        '##### Table Of Contents\n[Description](#Description)\n[Installiation](#Installiation)\n[Usage](#Usage)\n' +
-        '[License](#License)\n[Contributing](#Contributing)\n[Tests](#Tests)\n[Questions](#Questions)\n'+
-
-        '##Questions\n' + 'https://github.com/' + response.githubusername + '\nEmail: ' + response.email
+        content = '\n#'+ response.title + '\n\n' +
+        '#### Table Of Contents \n* [Description](#description) \n* [Installiation](#installiation) \n* [Usage](#usage) \n* [License](#license) \n* [Contributing](#contributing) \n* [Tests](#tests) \n* [Questions](#questions) \n\n' +
+        '### Description \n'+ response.description + '\n\n' +
+        '### Installiation \n' + response.instillationinstructions  + '\n\n' +
+        '### Usage \n' + response.usageinformation + '\n\n' +
+        '### License \n' + response.liscence + '\n\n' +
+        '### Contributing \n' + response.contributing + '\n\n' +
+        '### Tests \n' + response.test + '\n\n' +
+        '### Questions \n' + 'https://github.com/' + response.githubusername + '\n' + 'Email: ' + response.email + '\n'
 
         console.log(response.title);
         fs.writeFile('README.md', content, function(error, file){
